3260517b36ed55ee216484109e88b0082ca54402
bohenmian <bohenmian@gmail.com>
2018-08-26 22:00:23 +0800

    使用Zookeeper实现服务注册功能

---
+++ src/main/java/cn/edu/swpu/cins/nettydemo/chapter4/constant/RpcConstant.java
@@ -0,0 +1,12 @@
+package cn.edu.swpu.cins.nettydemo.chapter4.constant;
+
+public interface RpcConstant {
+
+    int ZK_SESSION_TIMEOUT = 5000;
+
+    String ZK_REGISTRY_PATH = "/registry";
+
+    String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
+
+
+}
